bcbd5ce655266f96bdb3cdcf6cda6dfb9bbb2f0c
sgarnica1 <sgarnica1902@gmail.com>
2025-08-13 17:16:31 -0600

    feat: update guests

--- src/utils/guests.ts
+++ src/utils/guests.ts
@@ -1,304 +1,312 @@
-import { GuestsType } from './types'
+import { GuestsType } from './types';
 
 export const GUESTS: GuestsType = {
-  "david-carmen-mayra": {
-    name: "David, Carmen & Mayra",
-    members: ["David", "Carmen", "Mayra"],
-  },
-  "david-diana": {
-    name: "David & Diana",
-    members: ["David", "Diana"],
-  },
-  "tia-lupe": {
-    name: "Tia Lupe",
-    members: ["Tia Lupe"],
-  },
-  "jessie-diego-hugo": {
-    name: "Jessie, Diego & Hugo",
-    members: ["Jessie", "Diego", "Hugo"],
-  },
-  "jahzel": {
-    name: "Jahzel",
-    members: ["Jahzel"],
-  },
-  "cynthia": {
-    name: "Cynthia",
-    members: ["Cynthia"],
-  },
-  "sophie-jose": {
-    name: "Sophie & Jose",
-    members: ["Sophie", "Jose"],
-  },
-  "rene-norma-emiliano": {
-    name: "Rene, Norma & Emiliano",
-    members: ["Rene", "Norma", "Emiliano"],
-  },
-  "alexis-marianne": {
-    name: "Alexis & Marianne",
-    members: ["Alexis", "Marianne"],
-  },
-  "jose-luis-ysabel": {
-    name: "Jose Luis & Ysabel",
-    members: ["Jose Luis", "Ysabel"],
-  },
-  "christian": {
-    name: "Christian",
-    members: ["Christian"],
-  },
-  "jorge-brianda": {
-    name: "Jorge & Brianda",
-    members: ["Jorge", "Brianda"],
-  },
-  "jose-luis-andrea": {
-    name: "Jose Luis & Andrea",
-    members: ["Jose Luis", "Andrea"],
-  },
-  "eric-esme": {
-    name: "Eric & Esme",
-    members: ["Eric", "Esme"],
-  },
-  "carmen-morena-mayra": {
-    name: "Carmen, Morena & Mayra",
-    members: ["Carmen", "Morena", "Mayra"],
-  },
-  "ramon-lety": {
-    name: "Ramon & Lety",
-    members: ["Ramon", "Lety"],
-  },
-  "sergio-lore": {
-    name: "Sergio & Lore",
-    members: ["Sergio", "Lore"],
-  },
-  "lari": {
-    name: "Lari",
-    members: ["Lari"],
-  },
-  "eleazar-raquel": {
-    name: "Eleazar & Raquel",
-    members: ["Eleazar", "Raquel"],
-  },
-  "samuel-ximena": {
-    name: "Samuel & Ximena",
-    members: ["Samuel", "Ximena"],
-  },
-  "aaron-fabi": {
-    name: "Aaron & Fabi",
-    members: ["Aaron", "Fabi"],
-  },
-  "cristobal": {
-    name: "Cristobal",
-    members: ["Cristobal"],
-  },
-  "ceci": {
-    name: "Ceci",
-    members: ["Ceci"],
-  },
-  "matias": {
-    name: "Matias",
-    members: ["Matias"],
-  },
-  "karlette-jimmy": {
-    name: "Karlette & Jimmy",
-    members: ["Karlette", "Jimmy"],
-  },
-  "alice": {
-    name: "Alice",
-    members: ["Alice"],
-  },
-  "gonzalo-hannia": {
-    name: "Gonzalo & Hannia",
-    members: ["Gonzalo", "Hannia"],
-  },
-  "brayan-gis": {
-    name: "Brayan & Gis",
-    members: ["Brayan", "Gis"],
-  },
-  "armando-mylene": {
-    name: "Armando & Mylene",
-    members: ["Armando", "Mylene"],
-  },
-  "sergio-dafne": {
-    name: "Sergio & Dafne",
-    members: ["Sergio", "Dafne"],
-  },
-  "fernando-caro": {
-    name: "Fernando & Caro",
-    members: ["Fernando", "Caro"],
-  },
-  "daniel-denisse": {
-    name: "Daniel & Denisse",
-    members: ["Daniel", "Denisse"],
-  },
-  "carlos-ana": {
-    name: "Carlos & Ana",
-    members: ["Carlos", "Ana"],
-  },
-  "john-dana": {
-    name: "John & Dana",
-    members: ["John", "Dana"],
-  },
-  "pablo-mich": {
-    name: "Pablo & Mich",
-    members: ["Pablo", "Mich"],
-  },
-  "leo-irma": {
-    name: "Leo & Irma",
-    members: ["Leo", "Irma"],
-  },
-  "david-toni": {
-    name: "David & Toni",
-    members: ["David", "Toni"],
-  },
-  "lalo-vero": {
-    name: "Lalo & Vero",
-    members: ["Lalo", "Vero"],
-  },
-  "luis-karen": {
-    name: "Luis & Karen",
-    members: ["Luis", "Karen"],
-  },
-  "cindy-tato": {
-    name: "Cindy & Tato",
-    members: ["Cindy", "Tato"],
-  },
-  "sandra-cesar": {
-    name: "Sandra & Cesar",
-    members: ["Sandra", "Cesar"],
-  },
-  "raquel-charlie-ayrton-michelle": {
-    name: "Raquel, Charlie, Ayrton & Michelle",
-    members: ["Raquel", "Charlie", "Ayrton", "Michelle"],
-  },
-  "mikey": {
-    name: "Mikey",
-    members: ["Mikey"],
-  },
-  "hannah-ivan": {
-    name: "Hannah & Ivan",
-    members: ["Hannah", "Ivan"],
-  },
-  "melody-corey": {
-    name: "Melody & Corey",
-    members: ["Melody", "Corey"],
-  },
-  "paul-lydia": {
-    name: "Paul & Lydia",
-    members: ["Paul", "Lydia"],
-  },
-  "joy-mark-sam": {
-    name: "Joy, Mark & Sam",
-    members: ["Joy", "Mark", "Sam"],
-  },
-  "sarah": {
-    name: "Sarah",
-    members: ["Sarah"],
-  },
-  "mike-jessie": {
-    name: "Mike & Jessie",
-    members: ["Mike", "Jessie"],
-  },
-  "danny-nancy": {
-    name: "Danny & Nancy",
-    members: ["Danny", "Nancy"],
-  },
-  "abue": {
-    name: "Abue",
-    members: ["Abue"],
-  },
-  "tia-yaya-tio-bilo-andrea-aranza": {
-    name: "Tia Yaya, Tio Bilo, Andrea & Aranza",
-    members: ["Tia Yaya", "Tio Bilo", "Andrea", "Aranza"],
-  },
-  "bai-michel": {
-    name: "Bai & Michel",
-    members: ["Bai", "Michel"],
-  },
-  "david-marilu": {
-    name: "David & Marilu",
-    members: ["David", "Marilu"],
-  },
-  "ivan-grace": {
-    name: "Ivan & Grace",
-    members: ["Ivan", "Grace"],
-  },
-  "beto-becky-ashley-megan": {
-    name: "Beto, Becky, Ashley & Megan",
-    members: ["Beto", "Becky", "Ashley", "Megan"],
-  },
-  "ramses": {
-    name: "Ramsés",
-    members: ["Ramsés", "+1"],
-  },
-  "isaac-iliana": {
-    name: "Isaac & Iliana",
-    members: ["Isaac", "Iliana"],
-  },
-  "rodolfo": {
-    name: "Rodolfo",
-    members: ["Rodolfo"],
-  },
-  "mich-saad": {
-    name: "Mich & Saad",
-    members: ["Mich", "Saad"],
-  },
-  "frank": {
-    name: "Frank",
-    members: ["Frank"],
-  },
-  "cami-juanito": {
-    name: "Cami & Juanito",
-    members: ["Cami", "Juanito"],
-  },
-  "marek": {
-    name: "Marek",
-    members: ["Marek"],
-  },
-  "glenn": {
-    name: "Glenn",
-    members: ["Glenn"],
-  },
-  "mauricio-judith": {
-    name: "Mauricio & Judith",
-    members: ["Mauricio", "Judith"],
-  },
-  "martha": {
-    name: "Martha",
-    members: ["Martha"],
-  },
-  "matt-layla": {
-    name: "Matt & Layla",
-    members: ["Matt", "Layla"],
-  },
-  "emanuel": {
-    name: "Emanuel",
-    members: ["Emanuel"],
-  },
-  "rebeca": {
-    name: "Rebeca",
-    members: ["Rebeca"],
-  },
-  "margarita-pablo": {
-    name: "Margarita & Pablo",
-    members: ["Margarita", "Pablo"],
-  },
-  "gustavo-eva": {
-    name: "Gustavo & Eva",
-    members: ["Gustavo", "Eva"],
-  },
-  "carlos-ana-estrada": {
-    name: "Carlos & Ana",
-    members: ["Carlos", "Ana"],
-  },
-  "isai-nancy": {
-    name: "Isaí & Nancy",
-    members: ["Isaí", "Nancy"],
-  },
-  "marco": {
-    name: "Marco",
-    members: ["Marco"],
-  },
-  "alejandro": {
-    name: "Alejandro",
-    members: ["Alejandro"],
+  'david-carmen-mayra': {
+    name: 'David, Carmen & Mayra',
+    members: ['David', 'Carmen', 'Mayra'],
+  },
+  'david-diana': {
+    name: 'David & Diana',
+    members: ['David', 'Diana'],
+  },
+  'tia-lupe': {
+    name: 'Tia Lupe',
+    members: ['Tia Lupe'],
+  },
+  'jessie-diego-hugo': {
+    name: 'Jessie, Diego & Hugo',
+    members: ['Jessie', 'Diego', 'Hugo'],
+  },
+  jahzel: {
+    name: 'Jahzel',
+    members: ['Jahzel'],
+  },
+  cynthia: {
+    name: 'Cynthia',
+    members: ['Cynthia'],
+  },
+  'sophie-jose': {
+    name: 'Sophie & Jose',
+    members: ['Sophie', 'Jose'],
+  },
+  'rene-norma-emiliano': {
+    name: 'Rene, Norma & Emiliano',
+    members: ['Rene', 'Norma', 'Emiliano'],
+  },
+  'alexis-marianne': {
+    name: 'Alexis & Marianne',
+    members: ['Alexis', 'Marianne'],
+  },
+  'jose-luis-ysabel': {
+    name: 'Jose Luis & Ysabel',
+    members: ['Jose Luis', 'Ysabel'],
+  },
+  christian: {
+    name: 'Christian',
+    members: ['Christian'],
+  },
+  'jorge-brianda': {
+    name: 'Jorge & Brianda',
+    members: ['Jorge', 'Brianda'],
+  },
+  'jose-luis-andrea': {
+    name: 'Jose Luis & Andrea',
+    members: ['Jose Luis', 'Andrea'],
+  },
+  'eric-esme': {
+    name: 'Eric & Esme',
+    members: ['Eric', 'Esme'],
+  },
+  'carmen-morena-mayra': {
+    name: 'Carmen, Morena & Mayra',
+    members: ['Carmen', 'Morena', 'Mayra'],
+  },
+  'ramon-lety': {
+    name: 'Ramon & Lety',
+    members: ['Ramon', 'Lety'],
+  },
+  'sergio-lore': {
+    name: 'Sergio & Lore',
+    members: ['Sergio', 'Lore'],
+  },
+  lari: {
+    name: 'Lari',
+    members: ['Lari'],
+  },
+  'eleazar-raquel': {
+    name: 'Eleazar & Raquel',
+    members: ['Eleazar', 'Raquel'],
+  },
+  'samuel-ximena': {
+    name: 'Samuel & Ximena',
+    members: ['Samuel', 'Ximena'],
+  },
+  'aaron-fabi': {
+    name: 'Aaron & Fabi',
+    members: ['Aaron', 'Fabi'],
+  },
+  cristobal: {
+    name: 'Cristobal',
+    members: ['Cristobal'],
+  },
+  ceci: {
+    name: 'Ceci',
+    members: ['Ceci'],
+  },
+  matias: {
+    name: 'Matias',
+    members: ['Matias'],
+  },
+  'karlette-jimmy': {
+    name: 'Karlette & Jimmy',
+    members: ['Karlette', 'Jimmy'],
+  },
+  alice: {
+    name: 'Alice',
+    members: ['Alice'],
+  },
+  'gonzalo-hannia': {
+    name: 'Gonzalo & Hannia',
+    members: ['Gonzalo', 'Hannia'],
+  },
+  'brayan-gis': {
+    name: 'Brayan & Gis',
+    members: ['Brayan', 'Gis'],
+  },
+  'armando-mylene': {
+    name: 'Armando & Mylene',
+    members: ['Armando', 'Mylene'],
+  },
+  'sergio-dafne': {
+    name: 'Sergio & Dafne',
+    members: ['Sergio', 'Dafne'],
+  },
+  'fernando-caro': {
+    name: 'Fernando & Caro',
+    members: ['Fernando', 'Caro'],
+  },
+  'daniel-denisse': {
+    name: 'Daniel & Denisse',
+    members: ['Daniel', 'Denisse'],
+  },
+  'carlos-ana': {
+    name: 'Carlos & Ana',
+    members: ['Carlos', 'Ana'],
+  },
+  'john-dana': {
+    name: 'John & Dana',
+    members: ['John', 'Dana'],
+  },
+  'pablo-mich': {
+    name: 'Pablo & Mich',
+    members: ['Pablo', 'Mich'],
+  },
+  'leo-irma': {
+    name: 'Leo & Irma',
+    members: ['Leo', 'Irma'],
+  },
+  'david-toni': {
+    name: 'David & Toni',
+    members: ['David', 'Toni'],
+  },
+  'lalo-vero': {
+    name: 'Lalo & Vero',
+    members: ['Lalo', 'Vero'],
+  },
+  'luis-karen': {
+    name: 'Luis & Karen',
+    members: ['Luis', 'Karen'],
+  },
+  'cindy-tato': {
+    name: 'Cindy & Tato',
+    members: ['Cindy', 'Tato'],
+  },
+  'sandra-cesar': {
+    name: 'Sandra & Cesar',
+    members: ['Sandra', 'Cesar'],
+  },
+  'raquel-charlie-ayrton-michelle': {
+    name: 'Raquel, Charlie, Ayrton & Michelle',
+    members: ['Raquel', 'Charlie', 'Ayrton', 'Michelle'],
+  },
+  mikey: {
+    name: 'Mikey',
+    members: ['Mikey'],
+  },
+  'hannah-ivan': {
+    name: 'Hannah & Ivan',
+    members: ['Hannah', 'Ivan'],
+  },
+  'melody-corey': {
+    name: 'Melody & Corey',
+    members: ['Melody', 'Corey'],
+  },
+  'paul-lydia': {
+    name: 'Paul & Lydia',
+    members: ['Paul', 'Lydia'],
+  },
+  'joy-mark-sam': {
+    name: 'Joy, Mark & Sam',
+    members: ['Joy', 'Mark', 'Sam'],
+  },
+  sarah: {
+    name: 'Sarah',
+    members: ['Sarah'],
+  },
+  'mike-jessie': {
+    name: 'Mike & Jessie',
+    members: ['Mike', 'Jessie'],
+  },
+  'danny-nancy': {
+    name: 'Danny & Nancy',
+    members: ['Danny', 'Nancy'],
+  },
+  abue: {
+    name: 'Abue',
+    members: ['Abue'],
+  },
+  'tia-yaya-tio-bilo-andrea-aranza': {
+    name: 'Tia Yaya, Tio Bilo, Andrea & Aranza',
+    members: ['Tia Yaya', 'Tio Bilo', 'Andrea', 'Aranza'],
+  },
+  'bai-michel': {
+    name: 'Bai & Michel',
+    members: ['Bai', 'Michel'],
+  },
+  'david-marilu': {
+    name: 'David & Marilu',
+    members: ['David', 'Marilu'],
+  },
+  'ivan-grace': {
+    name: 'Ivan & Grace',
+    members: ['Ivan', 'Grace'],
+  },
+  'beto-becky-ashley-megan': {
+    name: 'Beto, Becky, Ashley & Megan',
+    members: ['Beto', 'Becky', 'Ashley', 'Megan'],
+  },
+  ramses: {
+    name: 'Ramsés',
+    members: ['Ramsés', '+1'],
+  },
+  'isaac-iliana': {
+    name: 'Isaac & Iliana',
+    members: ['Isaac', 'Iliana'],
+  },
+  rodolfo: {
+    name: 'Rodolfo',
+    members: ['Rodolfo'],
+  },
+  'mich-saad': {
+    name: 'Mich & Saad',
+    members: ['Mich', 'Saad'],
+  },
+  frank: {
+    name: 'Frank',
+    members: ['Frank'],
+  },
+  'cami-juanito': {
+    name: 'Cami & Juanito',
+    members: ['Cami', 'Juanito'],
+  },
+  marek: {
+    name: 'Marek',
+    members: ['Marek'],
+  },
+  glenn: {
+    name: 'Glenn',
+    members: ['Glenn'],
+  },
+  'mauricio-judith': {
+    name: 'Mauricio & Judith',
+    members: ['Mauricio', 'Judith'],
+  },
+  martha: {
+    name: 'Martha',
+    members: ['Martha'],
+  },
+  'matt-layla': {
+    name: 'Matt & Layla',
+    members: ['Matt', 'Layla'],
+  },
+  emanuel: {
+    name: 'Emanuel',
+    members: ['Emanuel'],
+  },
+  rebeca: {
+    name: 'Rebeca',
+    members: ['Rebeca'],
+  },
+  'margarita-pablo': {
+    name: 'Margarita & Pablo',
+    members: ['Margarita', 'Pablo'],
+  },
+  'gustavo-eva': {
+    name: 'Gustavo & Eva',
+    members: ['Gustavo', 'Eva'],
+  },
+  'carlos-ana-estrada': {
+    name: 'Carlos & Ana',
+    members: ['Carlos', 'Ana'],
+  },
+  'isai-nancy': {
+    name: 'Isaí & Nancy',
+    members: ['Isaí', 'Nancy'],
+  },
+  marco: {
+    name: 'Marco',
+    members: ['Marco'],
+  },
+  alejandro: {
+    name: 'Alejandro',
+    members: ['Alejandro'],
+  },
+  'richo-miriam': {
+    name: 'Richo & Miriam',
+    members: ['Richo', 'Miriam'],
+  },
+  'chema-bolita': {
+    name: 'Chema & Bolita',
+    members: ['Chema', 'Bolita'],
   },
 };
